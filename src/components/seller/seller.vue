fc20477c9912f4316e33e090e8c9de81b903021b
yozosann <ytj123yue_@126.com>
2017-03-07 18:43:25 +0800

    seller组件

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,15 +1,152 @@
 <template>
-  <div>
-      I am seller!
-  </div>
+  <div class="seller" ref="seller">
+    <div class="seller-content">
+      <div class="overview">
+        <h1 class="title">{{seller.name}}</h1>
+        <div class="desc border-1px">
+          <star :size="36" :score="seller.score"></star>
+          <span class="text">({{seller.ratingCount}})</span>
+          <span class="text">月售{{seller.sellCount}}单</span>
+        </div>
+        <ul class="remark">
+          <li class="block">
+            <h2>起送价</h2>
+            <div class="content">
+              <span class="stress">{{seller.minPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>商家配送</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>平均配送时间</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryTime}}</span>元
+            </div>
+          </li>
+        </ul>
+        <div class="favorite" @click="toggleFavorite">
+          <span class="icon-favorite" :class="{'active':favorite}"></span>
+          <span class="text">{{favoriteText}}</span>
+        </div>
+      </div>
+      <split></split>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper border-1px">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item border-1px" v-for="item in seller.supports">
+            <icon :size="16" :iconType="item.type" :colorType="3"></icon>
+            <span class="text">{{item.description}}</span>
+          </li>
+        </ul>
+      </div>
+      <split v-show="seller.pics && seller.pics.length"></split>
+      <div class="pics" v-show="seller.pics && seller.pics.length">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper" ref="pic-wrapper">
+          <ul class="pic-list" ref="pic-list">
+            <li class="pic-item" v-for="pic in seller.pics">
+              <img :src="pic" width="120" height="90">
+            </li>
+          </ul>
+          </ul>
+        </div>
+      </div>
+      <split></split>
+      <div class="info">
+        <h1 class="title border-1px">商家信息</h1>
+        <ul>
+          <li class="info-item border-1px" v-for="info in seller.infos">{{info}}</li>
+        </ul>
+      </div>
+    </div>
 </template>
 
 <script>
-  export default {};
+  import star from 'components/star/star.vue';
+  import split from 'components/split/split.vue';
+  import icon from 'components/icon/icon.vue';
+  import BScroll from 'better-scroll';
+  import { saveToLocal, loadFromLocal } from 'common/js/store';
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        favorite: (() => {
+          return loadFromLocal(this.seller.id, 'favorite', false);
+        })()
+      };
+    },
+    computed: {
+      favoriteText() {
+        return this.favorite ? '收藏' : '未收藏';
+      }
+    },
+    components: {
+      star,
+      split,
+      icon
+    },
+    watch: {
+      'seller'() {
+        this.$nextTick(() => {
+          this._initScroll();
+          this._initPicScroll();
+        });
+      }
+    },
+    mounted() {
+      this._initScroll();
+      this._initPicScroll();
+    },
+    methods: {
+      _initScroll() {
+        if (!this.scroll) {
+          this.scroll = new BScroll(this.$refs.seller, {
+            click: true
+          });
+        } else {
+          this.scroll.refresh();
+        }
+      },
+      _initPicScroll() {
+        if (this.seller.pics) {
+          let picWidth = 120;
+          let margin = 6;
+          let width = (picWidth + margin) * this.seller.pics.length - margin;
+          this.$refs['pic-list'].style.width = width + 'px';
+          if (!this.picScroll) {
+            this.picScroll = new BScroll(this.$refs['pic-wrapper'], {
+              scrollX: true
+            });
+          } else {
+            this.picScroll.refresh();
+          }
+        }
+      },
+      toggleFavorite(event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.favorite = !this.favorite;
+        saveToLocal(this.seller.id, 'favorite', this.favorite);
+      }
+    }
+  };
 
 </script>
 
 <style lang="stylus">
-
+  @import './seller.styl';
 </style>
-
