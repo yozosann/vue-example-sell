79e15ef188db0e7ec40936564ac5b4ad776e1504
yozosann <ytj123yue_@126.com>
2017-03-07 18:43:10 +0800

    rating组件

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ratings">
+  <div class="ratings" ref="ratings">
     <div class="ratings-content">
       <div class="overview">
         <div class="overview-left">
@@ -25,15 +25,42 @@
         </div>
       </div>
       <split></split>
-      <ratingselect></ratingselect>
+      <ratingselect :father="self" :active-options="activeOptions" :desc="desc" :ratings="ratings"></ratingselect>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in ratings" class="rating-item border-1px">
+            <div class="avatar">
+              <img :src="rating.avatar" width="28px" height="28px">
+            </div>
+            <div class="content">
+              <div class="name">{{rating.username}}</div>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                <span class="icon-thumb_up"></span>
+                <span v-for="item in rating.recommend" class="item">{{item}}</span>
+              </div>
+              <div class="time">{{rating.rateTime | formatDate}}</div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import { formatDate } from 'common/js/date';
   import star from 'components/star/star.vue';
   import split from 'components/split/split.vue';
   import ratingselect from 'components/ratingselect/ratingselect.vue';
+  import BScroll from 'better-scroll';
+
+  const ALL = 2;
+  const ERR_OK = 0;
 
   export default {
     props: {
@@ -41,10 +68,63 @@
         type: Object
       }
     },
+    data() {
+      return {
+        ratings: [],
+        activeOptions: {
+          selectType: ALL,
+          onlyContent: true
+        },
+        desc: {
+          all: '全部',
+          positive: '满意',
+          negative: '不满意'
+        },
+        self: this
+      };
+    },
+    created() {
+      this.$http.get('/api/ratings').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.ratings = response.data;
+        }
+        this.$nextTick(() => {
+          this.scroll = new BScroll(this.$refs.ratings, {
+            click: true
+          });
+        });
+      });
+    },
+    mounted() {
+      this.$on('refresh', () => {
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      });
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time);
+        return formatDate(date, 'yyyy-MM-dd hh:mm');
+      }
+    },
     components: {
       star,
       split,
       ratingselect
+    },
+    methods: {
+      needShow(type, text) {
+        if (this.activeOptions.onlyContent && !text) {
+          return false;
+        }
+        if (this.activeOptions.selectType === ALL) {
+          return true;
+        } else {
+          return type === this.activeOptions.selectType;
+        }
+      }
     }
   };
 
